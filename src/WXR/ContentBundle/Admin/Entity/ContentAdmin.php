961a5fb5577c3443ac9752b2032259f957d480ce
Lionel Gaillard <lionel@wxrstudios.com>
2012-10-19 17:35:05 +0200

    Added ContentAdmin

---
+++ src/WXR/ContentBundle/Admin/Entity/ContentAdmin.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace WXR\ContentBundle\Admin\Entity;
+
+use WXR\ContentBundle\Admin\Model\ContentAdmin as BaseContentAdmin;
+
+class ContentAdmin extends BaseContentAdmin
+{
+}
---
+++ src/WXR/ContentBundle/Admin/Model/ContentAdmin.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WXR\ContentBundle\Admin\Model;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+
+abstract class ContentAdmin extends Admin
+{
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('description', null, array('required' => false))
+            ->add('title', null, array('required' => false))
+            ->add('content', null, array('required' => false))
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('description')
+            ->add('title')
+            ->add('content')
+        ;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('description')
+        ;
+    }
+}
